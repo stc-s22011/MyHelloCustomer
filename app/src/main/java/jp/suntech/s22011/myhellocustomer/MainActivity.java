61bcef2bbfd8a9e02edd77509cfc2eb15eedd7fe
stc-s22011 <137125381+stc-s22011@users.noreply.github.com>
2023-07-04 10:36:12 +0900

    MainActivity.java

--- app/src/main/java/jp/suntech/s22011/myhellocustomer/MainActivity.java
+++ app/src/main/java/jp/suntech/s22011/myhellocustomer/MainActivity.java
@@ -20,19 +20,19 @@ public class MainActivity extends AppCompatActivity {
     private class HelloListener implements View.OnClickListener{
        @Override
         public void onClick(View view) {
-            EditText input1 = findViewById(R.id.etLive);
+            EditText input1 = findViewById(R.id.etAd);
             EditText input2 = findViewById(R.id.etName);
 
             int id = view.getId();
 
-            if(id==R.id.btClick) {
+            if(id==R.id.btConfirm) {
                 String inputStr1 = input1.getText().toString();
                 String inputStr2 = input2.getText().toString();
                 String show = inputStr2 + "にお住いの" + inputStr1 + "さん、こんにちは！";
                 Toast.makeText(MainActivity.this, show, Toast.LENGTH_LONG).show();
             }
             //クリア
-            else if(id==R.id.btLiveclear) {
+            else if(id==R.id.btAdClear) {
                 input1.setText("");
             }
             //クリア
